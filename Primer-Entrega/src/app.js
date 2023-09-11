204bbdf9504b886c0f9a4fd4793fcf89c3ea78d8
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-09-11 02:32:40 -0300

    ruta para petiones carts

--- Primer-Entrega/src/app.js
+++ Primer-Entrega/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { productsRoutes } from './routes/products.routes.js';
+import { cartsRoutes } from './routes/carts.routes.js';
 
 const port = 8080;
 const app = express();
@@ -8,7 +9,7 @@ app.use(express.json());
 
 //rutas
 app.use("/api/products", productsRoutes);
-
+app.use("/api/carts", cartsRoutes);
 
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`);
