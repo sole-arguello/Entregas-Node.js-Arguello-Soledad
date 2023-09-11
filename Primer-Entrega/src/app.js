890adc84a23bc813f1e7298858cd43ffaca8cf77
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-09-11 02:41:34 -0300

    corrijo archivo de ruta

--- Primer-Entrega/src/app.js
+++ Primer-Entrega/src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import { productsRoutes } from './routes/products.routes.js';
-import { cartsRoutes } from './routes/carts.routes.js';
+import { cartsRouter } from './routes/carts.routes.js';
 
 const port = 8080;
 const app = express();
@@ -9,7 +9,8 @@ app.use(express.json());
 
 //rutas
 app.use("/api/products", productsRoutes);
-app.use("/api/carts", cartsRoutes);
+app.use("/api/carts", cartsRouter)
+
 
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`);
