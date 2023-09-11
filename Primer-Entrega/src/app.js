b17b7c1c4d25b1f12fe7e4ae89319f58aab86d7e
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-09-11 01:48:19 -0300

    agregue la ruta de peticion de productos

--- Primer-Entrega/src/app.js
+++ Primer-Entrega/src/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { productsRoutes } from './routes/products.routes';
+import { productsRoutes } from './routes/products.routes.js';
 
 const port = 8080;
 const app = express();
+app.use(express.json());
 
 //rutas
 app.use("/api/products", productsRoutes);
