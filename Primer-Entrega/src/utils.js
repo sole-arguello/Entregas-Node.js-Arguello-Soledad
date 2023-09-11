e933c8dfc627700a9dc380d952a130e9a6bd4ac0
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-09-11 01:39:49 -0300

    creo archivo utils para conectar los servicios

---
+++ Primer-Entrega/src/utils.js
@@ -0,0 +1,7 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+export const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+//dentro del proyecto ejecuto node src/utils.js
+console.log('__dirname',__dirname);
