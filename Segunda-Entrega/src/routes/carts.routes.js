94059056e7a209392411c768124fca672fdf0551
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-10-08 19:41:29 -0300

    obtengo la relacion del carrito con el producto

--- Segunda-Entrega/src/routes/carts.routes.js
+++ Segunda-Entrega/src/routes/carts.routes.js
@@ -19,14 +19,14 @@ router.get("/", async (req, res) => {
   router.get("/:cid", async (req, res) => {
     try {
       const idcarts = req.params.cid; //obtengo el parametro cid de la URL
+      //tarigo el caarito por medio de la populacion
       const carts = await cartsService.getCartsId(idcarts);
-      console.log('Carts 2',carts);
-      // const cart = carts.find((cart) => cart.id === idcarts); //busco el carrito por id
-      // console.log('cart 3', cart)
       if(carts){
         res.json({ message: "Carrito encontrado", data: carts });
+      }else{
+        res.json({ status: "error", message: "Carrito no encontrado"});
       }
-      res.json({ status: "error", message: "Carrito no encontrado"});
+      
     } catch (error) {
       res.json({ status: "error", message: error.message });
     }
@@ -41,8 +41,6 @@ router.get("/", async (req, res) => {
       res.json({ status: "error", message: error.message });
     }
   });
-
-  //6521eb2e36c88f2f66806387 cid //6521e3a17e9dfd915c36441b pid
   //http://localhost:8080/api/carts/:cid/products/:pid para agregar productos al carrito
   router.post("/:cid/products/:pid", async (req, res) => {
     try {
