c86e724a598e3c422a478dd5ba1c71bf035f0fde
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-10-08 17:19:01 -0300

    cambio nombre DB

--- Segunda-Entrega/src/config/dbConnection.js
+++ Segunda-Entrega/src/config/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const url = 'mongodb+srv://soledadar:g04D4zMd9O4y2GvK@cluster0.njbseut.mongodb.net/ecommerceDB?retryWrites=true&w=majority'
+        const url = 'mongodb+srv://soledadar:g04D4zMd9O4y2GvK@cluster0.njbseut.mongodb.net/tiendaDB?retryWrites=true&w=majority'
         await mongoose.connect(url);
         console.log('Conectado a la base de datos');
     } catch (error) {
