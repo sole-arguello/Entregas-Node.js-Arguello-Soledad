4049f100a07ae212c628bf597dab68d423afd3a3
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-10-10 14:39:08 -0300

    corrijo modelo

--- Segunda-Entrega/src/dao/mongo/models/carts.model.js
+++ Segunda-Entrega/src/dao/mongo/models/carts.model.js
@@ -6,16 +6,16 @@ const cartsSchema = new mongoose.Schema({
 
     products: {//la variable que tiene la populacion
         type: [//tipo array
-            {//id del producto igual que la DB
-                _id: {//populacion
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "products",//donde esta almacenado los productos
-                },
-                quantity: {
-                    type: Number,
-                    required: true
+                {//id del producto igual que la DB
+                    productId: {//populacion
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref: "products",//donde esta almacenado los productos
+                    },
+                    quantity: {
+                        type: Number,
+                        required: true
+                    }
                 }
-            }
         ],
         default: []//para cuando se cree los productos se cree el carrito vacio
     }
