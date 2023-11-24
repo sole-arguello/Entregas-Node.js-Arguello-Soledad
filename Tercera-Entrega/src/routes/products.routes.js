5e097f960371ebbafb0a2afb7f430ed5bc63e57b
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-11-23 22:08:01 -0300

    agrego validacion jwt

--- Tercera-Entrega/src/routes/products.routes.js
+++ Tercera-Entrega/src/routes/products.routes.js
@@ -1,6 +1,6 @@
 import {Router } from 'express';
 import { ProductsController } from '../controller/products.controller.js'; 
-import { authorization } from '../middlewares/auth.js';
+import { authorization, jwtAuth } from '../middlewares/auth.js';
 
 
 const router = Router();
@@ -11,10 +11,10 @@ router.get('/:id', ProductsController.getProductsId)
 
 //todas estas rutas llevan autorizacion
 //localhost:8080/api/products
-router.post('/', authorization(['admin']), ProductsController.createProduct)
+router.post('/', jwtAuth, authorization(['admin']), ProductsController.createProduct)
 
-router.put('/:id', authorization(['admin']), ProductsController.updateProduct)
+router.put('/:id', jwtAuth,  authorization(['admin']), ProductsController.updateProduct)
 
-router.delete('/:id', authorization(['admin']), ProductsController.deleteProduct)
+router.delete('/:id', jwtAuth, authorization(['admin']), ProductsController.deleteProduct)
 
 export { router as productsRouter }
