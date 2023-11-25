2fc4e72b19fcf9e7c7e73e7c5954131889261bf9
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-11-25 17:35:40 -0300

    corrijo codigo

--- Tercera-Entrega/src/dao/dto/user.dto.js
+++ Tercera-Entrega/src/dao/dto/user.dto.js
@@ -5,7 +5,6 @@ export class UsersDto {
         (this.last_name = userInfo.last_name),
         (this.email = userInfo.email),
         (this.age = userInfo.age),
-        (this.password = userInfo.password),
         (this.role = userInfo.role),
         (this.cart = userInfo.cart);
     }
