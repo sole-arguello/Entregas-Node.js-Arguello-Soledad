b614ac1961254e351cab501b4de8f6098cd56618
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-11-25 14:04:04 -0300

    creo archivo dto

---
+++ Tercera-Entrega/src/dao/dto/user.dto.js
@@ -0,0 +1,12 @@
+export class UsersDto {
+    constructor(userInfo) {
+        this.full_name = `${userInfo.first_name} ${userInfo.last_name}`.toUpperCase();
+        (this.first_name = userInfo.first_name),
+        (this.last_name = userInfo.last_name),
+        (this.email = userInfo.email),
+        (this.age = userInfo.age),
+        (this.password = userInfo.password),
+        (this.role = userInfo.role),
+        (this.cart = userInfo.cart);
+    }
+  }
