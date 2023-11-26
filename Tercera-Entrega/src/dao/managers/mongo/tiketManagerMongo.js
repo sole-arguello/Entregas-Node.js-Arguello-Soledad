f4123f3574bbfd34612071a7a10460b49ed0bc0f
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-11-26 19:37:01 -0300

    creo el manager Tiket

---
+++ Tercera-Entrega/src/dao/managers/mongo/tiketManagerMongo.js
@@ -0,0 +1,40 @@
+import { tiketModel } from "../models/tiket.model.js";
+
+export class TiketManagerMongo {
+    constructor(){
+        this.model = tiketModel
+    }
+
+    async createTicket(tiketBody){
+        try {
+            const tiket = await this.model.create({tiketBody});
+            console.log('createTiket con exito', tiket);
+            return tiket
+        } catch (error) {
+            console.log('Error en manager createTiket', error.message);
+            throw new Error('No se pudo crear el tiket ', error.message);
+        }
+    }
+
+    async getTikets(){
+        try {
+            const tikets = await this.model.find().lean();
+            console.log('getTikets con exito', tikets);
+            return tikets 
+        } catch (error) {
+            console.log('Error en manager getTikets', error.message);
+            throw new Error('No se pudo obtener el listado de los tikets ', error.message);
+        } 
+    }
+
+    async getTicketById(id){
+        try {
+            const tikets = await this.model.findById(id).lean();
+            console.log('getTiketById con exito', tikets);
+            return tikets 
+        } catch (error) {
+            console.log('Error en manager getTiketById', error.message);
+            throw new Error('No se pudo obtener el tiket ', error.message);
+        }
+    }
+}
