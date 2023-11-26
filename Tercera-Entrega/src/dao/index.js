ac09afe09cc6be4113b0a8b5a6dd5362951aa85a
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-11-26 19:37:33 -0300

    agrego el tiketDao

--- Tercera-Entrega/src/dao/index.js
+++ Tercera-Entrega/src/dao/index.js
@@ -6,9 +6,11 @@ import { ProductsManagerMongo } from './managers/mongo/productsManagerMongo.js';
 import { CartsManagerMongo } from './managers/mongo/cartsManagerMongo.js'
 import { ChatManagerMongo } from "./managers/mongo/chatManagerMongo.js";
 import { UsersManagerMongo } from "./managers/mongo/usersManagerMongo.js";
+import { TiketManagerMongo } from "./managers/mongo/tiketManagerMongo.js";
 
 export const productsDao = new ProductsManagerMongo();
 export const cartsDao = new CartsManagerMongo();
 export const chatDao = new ChatManagerMongo();
 export const usersDao = new UsersManagerMongo();
+export const tiketDao = new TiketManagerMongo()
 
